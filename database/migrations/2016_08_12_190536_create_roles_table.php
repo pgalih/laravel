a141f374a3d5ff2c49ea16748220d079bc827eea
galih <galihpamungkascom@gmail.com>
2016-08-13 02:54:13 +0700

    Create initial migration

--- database/migrations/2016_08_12_190536_create_roles_table.php
+++ database/migrations/2016_08_12_190536_create_roles_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddDescriptionToTasks extends Migration
+class CreateRolesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -12,8 +12,11 @@ class AddDescriptionToTasks extends Migration
      */
     public function up()
     {
-        Schema::table('tasks', function ($table) {
-            $table->string('description');
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->unique();
+            $table->string('status');
+            $table->timestamps();
         });
     }
 
@@ -24,8 +27,6 @@ class AddDescriptionToTasks extends Migration
      */
     public function down()
     {
-        Schema::table('tasks', function ($table) {
-            $table->dropColumn('description');
-        });
+        Schema::drop('roles');
     }
 }
