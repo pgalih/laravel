9001bdb32d406c249cfa1035980d7530c746b466
pgalih <galihpamungkascom@gmail.com>
2016-08-12 18:54:00 +0700

    Modified master dashboard page & delete default migration from laravel

--- resources/views/dashboard/dashboard.blade.php
+++ resources/views/dashboard/dashboard.blade.php
@@ -5,55 +5,78 @@
 <head>
  <meta charset="utf-8">
 	<title> Dashboard</title>
-	<link href="//netdna.bootstrapcdn.com/bootstrap/3.1.0/css/bootstrap.min.css" rel="stylesheet">
-
-<script src="//netdna.bootstrapcdn.com/bootstrap/3.1.0/js/bootstrap.min.js"></script>
-		{!! Html::style('css/style.css') !!}
-		{!! Html::style('css/bootstrap-theme.css') !!}
-		{!! Html::style('css/bootstrap-theme.css.css') !!}
-		{!! Html::style('css/bootstrap-theme.min.css') !!}
-		{!! Html::style('css/bootstrap.css') !!}
-		{!! HTML::style('css/bootstrap.min.css') !!}
+{!! Html::style('css/style.css') !!}
+{!! Html::style('css/bootstrap-theme.css') !!}
+{!! Html::style('css/bootstrap-theme.css') !!}
+{!! Html::style('css/bootstrap-theme.min.css') !!}
+{!! Html::style('css/bootstrap.css') !!}
+{!! HTML::style('css/bootstrap.min.css') !!}
 </head>
 
 <body>
 
-login Form
-{!! Form::open() !!}
-{!! Form::text('name', @$name) !!}
-{!! Form::password('password') !!}
-{!! Form::submit('Send') !!}
-{!! Form::close() !!}
-
  <!-- nav -->
-		   <nav class="navbar navbar-inverse navbar-fixed-top">
-      <div class="container">
+<nav class="navbar navbar-inverse navbar-fixed-top">
+    <div class="container">
         <div class="navbar-header">
-          <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-            <span class="sr-only">Toggle navigation</span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-            <span class="icon-bar"></span>
-          </button>
-          <a class="navbar-brand" href="#">Project name</a>
+          	<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+	            <span class="sr-only">Toggle navigation</span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+	            <span class="icon-bar"></span>
+          	</button>
+          	<a class="navbar-brand" href="#">Project name</a>
         </div>
+        
         <div id="navbar" class="navbar-collapse collapse">
-          <form class="navbar-form navbar-right">
-            <div class="form-group">
-              <input type="text" placeholder="Email" class="form-control">
-            </div>
-            <div class="form-group">
-              <input type="password" placeholder="Password" class="form-control">
-            </div>
-            <button type="submit" class="btn btn-success">Sign in</button>
-          </form>
+         	{!! Form::open(array('class' => 'navbar-form navbar-right')) !!}
+            {!! Form::text('username', 'Username', array('class' => 'form-control')) !!}
+            {!! Form::password('password', array('class' => 'form-control')) !!}
+            {!! Form::submit('Send', array('class' => 'btn btn-success')) !!}
+	        {!! Form::close() !!}
         </div><!--/.navbar-collapse -->
-      </div>
-    </nav>
+    </div>
+</nav>
     <!-- /nav -->
 	
+<div class="container">
+ 	<div class="starter-template">
+        <h1>Bootstrap starter template</h1>
+        <p class="lead">Use this document as a way to quickly start any new project.<br> All you get is this text and a mostly barebones HTML document.</p>
+     </div>
+</div>
+
+<div class="panel panel-primary">
+  	<div class="panel-heading">
+    	<h3 class="panel-title">Tasklist</h3>
+  	</div>
+  	
+  	<div class="panel-body">
+	  	<div class="list-group">
+		  <a href="#" class="list-group-item active">Latihan</a>
+		  <a href="#" class="list-group-item">Membuat halaman PHP</a>
+		  <a href="#" class="list-group-item">Integrasi Database</a>
+		  <a href="#" class="list-group-item">Membuat halaman CSS</a>
+		  <a href="#" class="list-group-item">Mengumpulkan data</a>
+		</div>
+
+		
+		<nav aria-label="...">
+		 	<ul class="pager">
+			    <li class="previous"><a href="#"><span aria-hidden="true">&larr;</span> Sebelumnya</a></li>
+			    <li class="next"><a href="#">Selanjutnya <span aria-hidden="true">&rarr;</span></a></li>
+  			</ul>
+		</nav>
+  	</div>
+</div>
+
+
+
+
 	@yield('content')
 
+
+ <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 {!!Html::script('js/npm.js')!!}
 {!!Html::script('js/bootstrap.js')!!}
 
