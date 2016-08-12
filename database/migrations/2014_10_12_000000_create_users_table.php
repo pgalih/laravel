ce299ef02965f697374ddd1d45eed62439a59ee5
pgalih <galihpamungkascom@gmail.com>
2016-08-12 18:51:14 +0700

    Create Users & Tasklists Table

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,7 +14,11 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('username');
             $table->string('name');
+            $table->string('title');
+            $table->string('no_hp');
+            $table->text('jobdes');
             $table->string('email')->unique();
             $table->string('password', 60);
             $table->rememberToken();
